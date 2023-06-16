b37bfb428672dabf5db45f1ba139d33b372b0952
Geetika Jhanji <geetikajhanji11@gmail.com>
2023-06-16 23:48:55 +0530

    braintree delete

--- server/routes/braintree.js
+++ server/routes/braintree.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const brainTreeController = require("../controller/braintree");
 
-router.post("/braintree/get-token", brainTreeController.ganerateToken);
-router.post("/braintree/payment", brainTreeController.paymentProcess);
+// router.post("/braintree/get-token", brainTreeController.ganerateToken);
+// router.post("/braintree/payment", brainTreeController.paymentProcess);
 
 module.exports = router;
